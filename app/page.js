af88746df77ffe86666063b6bb81224ccde9d4df
Haq Nawaz <haq.hawaz@hiddenlogics.com>
2023-01-07 13:06:01 +0500

    For musa add new todo component added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import AddNewTodo from "./add-new-todo";
+
 export default function page() {
   return (
     <div>
       <div>
-        <input type="text" />
-        <button>Add</button>
+        <AddNewTodo />
         <ul style={{ listStyleTupe: "none", padding: 0 }}>
           <li style={{ padding: "5px 0" }}>Todo 1</li>
           <li style={{ padding: "5px 0" }}>Todo 2</li>
