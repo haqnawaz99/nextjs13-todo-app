f056aa8975a985a58efd068309c2a1d94e916467
haqnawaz <haqnawaz99@yahoo.com>
2023-01-17 12:04:34 +0500

    checked functionality added

--- app/todo.js
+++ app/todo.js
@@ -1,10 +1,28 @@
 "use client";
+
+import { useRouter } from "next/navigation";
+
+async function update(id, isDone, refresh) {
+  await fetch(`http://localhost:3001/api/todo/update`, {
+    method: "POST",
+    mode: "no-cors",
+    body: JSON.stringify({ id, isDone }),
+  });
+  refresh();
+}
+
 export default function Todo({ todo }) {
+  const router = useRouter();
+
   return (
     <div>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        onChange={(e) => update(todo.id, e.target.checked, router.refresh)}
+        checked={todo.isDone}
+      />
       {todo.name}
-      <button onChange={() => {}}>Delete</button>
+      <button>Delete</button>
     </div>
   );
 }
